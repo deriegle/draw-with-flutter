df2d99cafd356e38a864343fadfdb24bf1e0ca57
Devin Riegle <devin.riegle@joinroot.com>
2020-04-13 17:03:22 -0400

    Add JSON conversion for Line & Offset models

--- lib/models/line.dart
+++ lib/models/line.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:drawful_impl/main.dart';
 
 class Line {
   Color color;
@@ -26,5 +27,41 @@ class Line {
     }
 
     path = path;
+  }
+
+  Map<String, dynamic> toJSON() {
+    return {
+      'color': color.value,
+      'strokeWidth': strokeWidth,
+      'offsets': offsets.map((offset) => offset.toJSON()),
+    };
+  }
+
+  factory Line.fromJSON(Map<String, dynamic> json) {
+    var newLine = Line(
+      offsetsFromJSON(json['offsets']),
+      json['strokeWidth'] ?? MIN_POINT_SIZE,
+      Color(json['color']),
+    );
+
+    if (newLine.offsets.length > 0) {
+      newLine.getPathFromOffset();
+    }
+
+    return newLine;
+  }
+}
+
+List<Offset> offsetsFromJSON(List<dynamic> json) {
+  if (json == null) {
+    return List<Offset>();
+  }
+
+  return List.from(json).map((offset) => Offset(offset['x'], offset['y'])).toList();
+}
+
+extension OffsetJSON on Offset {
+  Map<String, dynamic> toJSON() {
+    return {'x': this.dx, 'y': this.dy};
   }
 }
